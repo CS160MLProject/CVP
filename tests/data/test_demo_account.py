e72bece71e01cf207c91b29983cf0280b804e8c1
Soma Yoshida <soma.y981029@gmail.com>
2021-03-08 11:03:19 -0800

    Implemented unit test.

--- tests/data/test_demo_account.py
+++ tests/data/test_demo_account.py
@@ -1,5 +1,16 @@
-def test_always_passes():
-    assert True
+"""Test of demo_account.py"""
+from cvp.data.demo_account import *
 
-def test_always_fails():
-    assert False
+TOKEN_SIZE = 14  # token length of output line
+
+
+def test_accounts_txt():
+    assert generate_accounts(), 'Error occoured when generating sample accounts'
+    with open(ACCOUNTS) as output_file:
+        counter = 0
+        while account := output_file.read():
+            counter += 1
+            tokens = account.split()
+            print(tokens)
+            assert len(tokens) == TOKEN_SIZE, f'Output\'s token size should be {TOKEN_SIZE}, returned {len(tokens)}'
+        assert counter == ACCOUNT_SIZE, f'Output file\'s account size should be {ACCOUNT_SIZE}, returned {counter}'

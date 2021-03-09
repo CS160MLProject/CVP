ebe6bad929c6979d707b2bea6c7105e46826b652
Soma Yoshida <soma.y981029@gmail.com>
2021-03-08 16:05:53 -0800

    Added Docstring to app.py

--- cvp/app.py
+++ cvp/app.py
@@ -8,11 +8,19 @@ app = Flask(__name__)
 
 @app.route('/')
 def homepage():
+    """
+    Homepage of this website with login and register option.
+    :return: homepage.html with title and body for demo.
+    """
     return render_template('homepage.html', title='this is title of homepage', body='option to register and login')
 
 
-@app.route('/register', methods=["GET", "POST"])
+@app.route('/register', methods=["POST"])
 def register_input():
+    """
+    Invoked when register button is clicked in homepage.
+    :return: registration page of uploading_of_document
+    """
     print(f'in register(), {request.method=}')
     if request.method == "POST":
         return render_template("uploading_of_document.html")
@@ -20,6 +28,11 @@ def register_input():
 
 @app.route('/register_confirm', methods=["GET", "POST"])
 def register_confirm():
+    """
+    Invoked when register button is clicked with information of email, password, and confirm password.
+    Obtain values from the user and validate email and password and confirm password.
+    :return:
+    """
     print(f'in register_confirm(), {request.method=}')
     if request.method == "POST":
         email = request.form.get('email')

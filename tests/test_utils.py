91744e4620a847d48ea33b260495ce1b17e904e7
Soma Yoshida <soma.y981029@gmail.com>
2021-04-02 20:36:28 -0700

    Added comment on test_utils.py.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,20 +1,29 @@
-import pytest
-import os
 from cvp.utils import *
 
 
 def test_sharing_qr():
+    """
+    Test if sharing_qr creates qr file in expected place.
+    """
     account_id = 12345
     qr_file = sharing_qr(account_id=account_id)
     assert os.path.exists(qr_file) is True, f'The qr file is not created in the expected directory.'
 
 
 def test_invalid_register_input():
+    """
+    Test if invalid_register_input return error message if there is any errors.
+    """
     valid_email_format = 'hello@me.com'
     valid_pass = valid_conf_pass = 'password'
     error = invalid_register_input(valid_email_format, valid_pass, valid_conf_pass)
-    assert error is None, f'Should return no error message for {valid_email_format=}, {valid_pass=}, {valid_conf_pass=}' \
+    assert error, f'Should return no error message for {valid_email_format=}, {valid_pass=}, {valid_conf_pass=}' \
                           f'returning {error}'
+
+    invalid_conf_pass = 'pass'
+    error = invalid_register_input(valid_email_format, valid_pass, invalid_conf_pass)
+    assert error, f'Should return error message for {valid_email_format=}, {valid_pass=}, {invalid_conf_pass=}' \
+                  f'returning {error}'
 
     invalid_email = 'this is invalid'
     error = invalid_register_input(invalid_email, valid_pass, valid_conf_pass)
@@ -23,6 +32,9 @@ def test_invalid_register_input():
 
 
 def test_valid_email():
+    """
+    Test if valid_email validate email as expected.
+    """
     valid_email_format = 'hello@me.com'
     invalid_email = 'this is invalid'
     assert valid_email(valid_email), f'Should return True for email {valid_email_format}'
@@ -30,6 +42,9 @@ def test_valid_email():
 
 
 def test_get_file_ext():
+    """
+    Test get_file_ext return classified file type as expected.
+    """
     image_file = 'image_file.png'
     pdf_file = 'pdf_file.pdf'
     other_file = 'other.exe'

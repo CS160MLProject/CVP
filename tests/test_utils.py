319c56fb4323da79b480782ad1cb5ab60487cb4d
Soma Yoshida <soma.y981029@gmail.com>
2021-04-02 17:08:17 -0700

    Implemented test function to test valid_email().

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -10,15 +10,20 @@ def test_sharing_qr():
 
 
 def test_invalid_register_input():
-    valid_email = 'hello@me.com'
+    valid_email_format = 'hello@me.com'
     valid_pass = valid_conf_pass = 'password'
-    error = invalid_register_input(valid_email, valid_pass, valid_conf_pass)
-    assert error is None, f'Should return no error message for {valid_email=}, {valid_pass=}, {valid_conf_pass=}' \
+    error = invalid_register_input(valid_email_format, valid_pass, valid_conf_pass)
+    assert error is None, f'Should return no error message for {valid_email_format=}, {valid_pass=}, {valid_conf_pass=}' \
                           f'returning {error}'
 
     invalid_email = 'this is invalid'
     error = invalid_register_input(invalid_email, valid_pass, valid_conf_pass)
-    assert error, f'Should return error message for {valid_email=}, {valid_pass=}, {valid_conf_pass=}' \
+    assert error, f'Should return error message for {valid_email_format=}, {valid_pass=}, {valid_conf_pass=}' \
                   f'returning {error}'
 
 
+def test_valid_email():
+    valid_email_format = 'hello@me.com'
+    invlid_email = 'this is invalid'
+    assert valid_email(valid_email), f'Should return True for email {valid_email_format}'
+    assert not valid_email(invlid_email), f'Should return Flase for email {invlid_email}'

bff32a7b0d448a012a3ffd1a009febab2029ce24
Soma Yoshida <soma.y981029@gmail.com>
2021-04-02 17:05:12 -0700

    Implemented test function for invalid_register_input().

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -9,4 +9,16 @@ def test_sharing_qr():
     assert os.path.exists(qr_file) is True, f'The qr file is not created in the expected directory.'
 
 
+def test_invalid_register_input():
+    valid_email = 'hello@me.com'
+    valid_pass = valid_conf_pass = 'password'
+    error = invalid_register_input(valid_email, valid_pass, valid_conf_pass)
+    assert error is None, f'Should return no error message for {valid_email=}, {valid_pass=}, {valid_conf_pass=}' \
+                          f'returning {error}'
+
+    invalid_email = 'this is invalid'
+    error = invalid_register_input(invalid_email, valid_pass, valid_conf_pass)
+    assert error, f'Should return error message for {valid_email=}, {valid_pass=}, {valid_conf_pass=}' \
+                  f'returning {error}'
+
 

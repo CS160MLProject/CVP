2e6c392ce5ab4e92041054f8af4b59c4daeca6a3
Soma Yoshida <soma.y981029@gmail.com>
2021-04-05 15:54:37 -0700

    Implemented unit test functions for register.

--- tests/test_app.py
+++ tests/test_app.py
@@ -20,3 +20,21 @@ def test_homepage_post(test_client):
     assert response.status_code == 200
 
 
+def test_register_get(test_client):
+    """
+    GIVEN a flask app configuration as test client
+    WHEN the '/' (homepage) is requested (GET)
+    THEN check that the response is valid
+    """
+    response = test_client.get('/register')
+    assert response.status_code == 200
+
+
+def test_register_post(test_client):
+    """
+    GIVEN a flask app configuration as test client
+    WHEN the '/' (homepage) is post (POST)
+    THEN check that the response is valid
+    """
+    response = test_client.post('/register')
+    assert response.status_code == 200

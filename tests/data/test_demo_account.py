fe3b3244dab39abe1cdec517ef92798881d536dd
Soma Yoshida <soma.y981029@gmail.com>
2021-04-20 11:15:13 -0700

    Added test case to validate the email in the accounts.txt.

--- tests/data/test_demo_account.py
+++ tests/data/test_demo_account.py
@@ -1,8 +1,9 @@
 """Test of demo_account.py"""
 # import target file
 import pytest
-
+import re
 from cvp.data.demo_account import *
+
 TOKEN_SIZE = 14  # token length of output line
 NAMES = 'dataset/raw/names.txt'
 ACCOUNTS = 'tests/data/test_accounts.txt'
@@ -26,4 +27,17 @@ def test_accounts_txt():
             assert tokens[11] < tokens[13], f'Should take second vaccine after first dose.'
             assert int(tokens[4]) <= 9999, f'Patient number should be less than or equal to 9999'
             assert int(tokens[4]) >= 1000, f'Patient number should be greater than or equal to 1000'
+            assert __valid_email(tokens[1]), f'Should return True for any email created in the accounts.txt'
         assert counter == ACCOUNT_SIZE, f'Output file\'s account size should be {ACCOUNT_SIZE}, returned {counter}'
+
+
+def __valid_email(email):
+    """
+    Validate entered email.
+    :param email: email address entered by user.
+    :return: True if this email is valid.
+    """
+    email_regex = r'^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'
+    if re.search(email_regex, email):  # if valid email
+        return True
+    return False

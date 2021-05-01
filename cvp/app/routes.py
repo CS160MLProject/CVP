709e3fb87a78e92b85c9a7fea1e466459137638e
Soma Yoshida <soma.y981029@gmail.com>
2021-05-01 14:56:08 -0700

    Bug fixing for email.

--- cvp/app/routes.py
+++ cvp/app/routes.py
@@ -176,7 +176,7 @@ def forgot_password():
                 token = encode_token(email, salt=recovery_key)
                 recover_url = url_for('reset_password', token=token, _external=True)
                 html = render_template('email/password_recovery.html', recover_url=recover_url)
-
+                print(html)
                 # send email with setup link
                 send_password_recovery_email(html, email)
                 return render_template('resend.html')
